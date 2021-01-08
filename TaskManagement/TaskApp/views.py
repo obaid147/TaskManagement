b605f5dcfa35f68fa773b29a9886e872db50ae29
Obaid <you@example.com>
2021-01-08 21:35:57 +0530

    Add New Task form Added

--- TaskManagement/TaskApp/views.py
+++ TaskManagement/TaskApp/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 
 def index(request):
     all_tasks = Task.objects.all()
+    form = TaskForm()
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            my_task = form.save()
+            print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+            messages.success(request, f"{my_task} Added!")
+        return redirect("/")
     context = {
         'all_tasks': all_tasks,
+        'form': form,
         'title': 'Manage Tasks',
         'heading': 'Task Management'
     }
