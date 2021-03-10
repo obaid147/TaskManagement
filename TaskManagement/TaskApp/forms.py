6c4b19d02687b9a0ef9f0ffca1c9d41351e1c15a
obaid <obaidfayazwani@gmail.com>
2021-03-10 10:25:51 +0530

    little changes in index and added placeholder

--- TaskManagement/TaskApp/forms.py
+++ TaskManagement/TaskApp/forms.py
@@ -3,6 +3,7 @@ from django import forms
 
 
 class TaskForm(forms.ModelForm):
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={"placeholder": "Enter Your Task"}))
 
     class Meta:
         model = Task
